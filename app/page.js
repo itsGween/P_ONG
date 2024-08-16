db642a28d9adae460d070ffd3744d66bc8dabd5e
IamGween <2718278@collegelacite.ca>
2024-08-15 22:44:00 -0400

    accepter les modification

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import CarouselWithText from "@/src/components/ui/CarouselWithText";
 import { Carousel } from "@/src/components/ui/apple-cards-carousel";
 import { HoverEffect } from "@/src/components/ui/card-hover-effect";
 import CARD_CONTENT from "@/src/constants/index";
+import Card from "@/src/components/ui/card";
 
 export default function Home() {
   return (
@@ -12,7 +13,11 @@ export default function Home() {
       <CarouselWithText />
       {/* <Carousel items={CARD_CONTENT} /> Passer directement CARD_CONTENT */}
 
-      <HoverEffect items={CARD_CONTENT} />
+      <div
+        style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+      >
+        <MusicCard />
+      </div>
     </div>
   );
 }
