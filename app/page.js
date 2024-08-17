0c3e79c745a70f90933464b5a1d332db2eb2524c
brydel <bsaounde@gmail.com>
2024-08-17 11:16:22 -0400

    modif entrante

--- app/page.js
+++ app/page.js
@@ -4,10 +4,8 @@ import CarouselWithText from "@/src/components/ui/CarouselWithText";
 import { Carousel } from "@/src/components/ui/apple-cards-carousel";
 import { HoverEffect } from "@/src/components/ui/card-hover-effect";
 import CARD_CONTENT from "@/src/constants/index";
-<<<<<<< HEAD
 import { Hero } from "@/src/components";
 import { Footer } from "@/src/components";
-=======
 import { CardDemo } from "@/src/components/ui/content_card";
 
 const items = [
@@ -18,24 +16,21 @@ const items = [
   { src: "/assets/image/coucher_de_soleil.jpg", title: "DIVERTISSEMENT" },
   { src: "/assets/image/champs_de_fleurs.jpg", title: "AUTRES" },
 ];
->>>>>>> 68159e284ad12ce5b98406a3bf59ac50cd131f83
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero/>
-      {/* <Carousel items={CARD_CONTENT} /> Passer directement CARD_CONTENT */}
-<<<<<<< HEAD
+      <CardDemo items={items} />
+      
 
-      <HoverEffect items={CARD_CONTENT} />
+      
 
       <Footer/>
-=======
-      {/* 
-      <HoverEffect items={CARD_CONTENT} /> */}
-      <CardDemo items={items} />
->>>>>>> 68159e284ad12ce5b98406a3bf59ac50cd131f83
+       
+      
+      
     </div>
     
 
