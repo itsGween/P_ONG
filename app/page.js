9e6aeccff3203fcc1e014d3b8cfb35d929910196
IamGween <2718278@collegelacite.ca>
2024-08-17 02:36:41 -0400

    ajustement

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import { Navbar } from "@/src/components";
+import custom from "@/src/styles/custom.css";
 import CarouselWithText from "@/src/components/ui/CarouselWithText";
 import { Carousel } from "@/src/components/ui/apple-cards-carousel";
 import { HoverEffect } from "@/src/components/ui/card-hover-effect";
 import CARD_CONTENT from "@/src/constants/index";
-<<<<<<< HEAD
 import { Hero } from "@/src/components";
 import { Footer } from "@/src/components";
-=======
 import { CardDemo } from "@/src/components/ui/content_card";
 
 const items = [
@@ -18,32 +17,16 @@ const items = [
   { src: "/assets/image/coucher_de_soleil.jpg", title: "DIVERTISSEMENT" },
   { src: "/assets/image/champs_de_fleurs.jpg", title: "AUTRES" },
 ];
->>>>>>> 68159e284ad12ce5b98406a3bf59ac50cd131f83
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Hero/>
-      {/* <Carousel items={CARD_CONTENT} /> Passer directement CARD_CONTENT */}
-<<<<<<< HEAD
+      <Hero />
 
-      <HoverEffect items={CARD_CONTENT} />
-
-      <Footer/>
-=======
-      {/* 
-      <HoverEffect items={CARD_CONTENT} /> */}
       <CardDemo items={items} />
->>>>>>> 68159e284ad12ce5b98406a3bf59ac50cd131f83
+
+      <Footer />
     </div>
-    
-
-    
-
-     
-
-
-    
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,17 @@
-import Navbar from "./Navbar";
 import Carousel from "./ui/apple-cards-carousel";
 import HoverEffect from "./ui/card-hover-effect";
 import CarouselWithText from "./ui/CarouselWithText";
-<<<<<<< HEAD
 import Hero from "./Hero";
-import Footer from "./Footer"
-
-export { Navbar, Carousel, HoverEffect, CarouselWithText, Hero, Footer};
-=======
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import CardDemo from "./ui/content_card";
 
-export { Navbar, Carousel, HoverEffect, CarouselWithText, CardDemo };
->>>>>>> 68159e284ad12ce5b98406a3bf59ac50cd131f83
+export {
+  Navbar,
+  Carousel,
+  HoverEffect,
+  CarouselWithText,
+  Hero,
+  Footer,
+  CardDemo,
+};
